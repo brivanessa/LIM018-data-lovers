03d739d5b616cf7994006a97ea592ed18136cb9b
Vanessa <vanessa@MacBook-Air-de-Vanessa.local>
2022-06-10 22:50:33 -0500

    Ocultando el primer elemento (section) del DOM

--- src/main.js
+++ src/main.js
@@ -2,12 +2,7 @@
 // import data from './data/lol/lol.js';
 import data from './data/athletes/athletes.js';
 const data1=data;
-console.log( data);
-
-
-//window.addEventListener('load', addElement()) // es lo msimo
-//window.addEventListener('load', function() {addElement()});  // es lo mismo y sirve mas para para agregar parametros/argumentos
-//document.body.onload = addElement; // es lo mismo
+//console.log(data);
 
 const addElement =(data1) =>{
     const fragment=document.createDocumentFragment();
@@ -27,10 +22,17 @@ const addElement =(data1) =>{
         const newSeccion = elementMain.cloneNode(true); //clonamos un nodo del DOM (el nodo es de la seccion existente) -TRUE es para clonar los hijos de ese nodo
         //parentMain.appendChild(newSeccion); // clonas un documeto y lo devuelves al final 
         newSeccion.querySelector("h4").textContent = item.team;
-        fragment.appendChild(newSeccion);
-        
+        fragment.appendChild(newSeccion);      
     });
-    parentMain.appendChild(fragment); //devuelves el fragment al final del documento
+    document.getElementById("athletes").style.display="none"; //para ocultar el primer elmento
+    parentMain.appendChild(fragment); //devuelves el fragment al final del documento   
 }
 
+
+
+
+
 window.addEventListener('load', addElement); 
+//window.addEventListener('load', addElement()) // es lo msimo
+//window.addEventListener('load', function() {addElement()});  // es lo mismo y sirve mas para para agregar parametros/argumentos
+//document.body.onload = addElement; // es lo mismo
