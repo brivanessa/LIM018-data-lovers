94bbf3e89542c337040827cd86d483ecf7a5b3ef
Vanessa <vanessa@MacBook-Air-de-Vanessa.local>
2022-06-13 11:06:11 -0500

    Borrando algunos comentarios y reduciendo código

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/athletes/athletes.js';
 //const data1=data;
 
@@ -8,22 +6,23 @@ import data from './data/athletes/athletes.js';
     const fragment=document.createDocumentFragment();
     //Crear nuevo div y añadirlo al elemento main
     const newDiv = document.createElement("div"); // crea un nuevo div
-    const newContent = document.createTextNode("El numero total de deportistas es:");  // y añade contenido
+    const newContent = document.createTextNode("El número total de deportistas es:");  // y añade contenido
     newDiv.appendChild(newContent); //añade texto al div creado.
     // añade el elemento creado y su contenido al DOM
-    const elementMain1 = document.getElementById("athletes"); // con el metodo getElementById devuelve una referencia del elemento seccion con id "athletes" (es traido del DOM)
-    const parentMain = elementMain1.parentNode; // Obtener una referencia al nodo padre
-    parentMain.insertBefore(newDiv, elementMain1); // 
+    const elementMain = document.getElementById("athletes"); // con el metodo getElementById devuelve una referencia del elemento seccion con id "athletes" (es traido del DOM)
+    const parentMain = elementMain.parentNode; // Obtener una referencia al nodo 
+    parentMain.insertBefore(newDiv, elementMain); // 
     
     data.athletes.forEach((item) => {
-        const elementMain = document.getElementById("athletes"); // con el metodo getElementById devuelve una referencia del elemento seccion con id "athletes" (es traido del DOM)
         //Clonar una seccion del DOM y añadirla
         //añade secciones clonadas del elemento seccion athletes
         const newSeccion = elementMain.cloneNode(true); //clonamos un nodo del DOM (el nodo es de la seccion existente) -TRUE es para clonar los hijos de ese nodo
         //parentMain.appendChild(newSeccion); // clonas un documeto y lo devuelves al final 
+        //console.log(item)
         newSeccion.querySelector("h4").textContent = item.team;
         newSeccion.querySelector("h5").textContent = item.name;
-        fragment.appendChild(newSeccion);      
+        fragment.appendChild(newSeccion);  
+          
     });
     document.getElementById("athletes").style.display="none"; //para ocultar el primer elmento
     parentMain.appendChild(fragment); //devuelves el fragment al final del documento   
