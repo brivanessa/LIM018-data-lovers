ff5b0a55a709ea3bb9bc0a5d57a7ecc80e592495
Vanessa <vanessa@MacBook-Air-de-Vanessa.local>
2022-06-13 23:36:14 -0500

    Hover para las casillas

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import data from './data/athletes/athletes.js';
+console.log(data);
 
     const fragment=document.createDocumentFragment();
     //Crear nuevo div y añadirlo al elemento main
@@ -6,7 +7,7 @@ import data from './data/athletes/athletes.js';
     const newContent = document.createTextNode("El número total de deportistas es:");  // y añade contenido
     newDiv.appendChild(newContent); //añade texto al div creado.
     // añade el elemento creado y su contenido al DOM
-    const elementMain = document.getElementById("athletes"); // con el metodo getElementById devuelve una referencia del elemento seccion con id "athletes" (es traido del DOM)
+    const elementMain = document.getElementById("athlete"); // con el metodo getElementById devuelve una referencia del elemento seccion con id "athletes" (es traido del DOM)
     const parentMain = elementMain.parentNode; // Obtener una referencia al nodo 
     parentMain.insertBefore(newDiv, elementMain); // 
     
@@ -18,8 +19,13 @@ import data from './data/athletes/athletes.js';
         //console.log(item)
         newSeccion.querySelector("h4").textContent = item.team;
         newSeccion.querySelector("h5").textContent = item.name;
+        newSeccion.querySelector("h6").textContent = item.medal;
+        newSeccion.querySelector("h7").textContent = item.sport;
+        newSeccion.querySelector("h8").textContent = item.gender;
+        newSeccion.querySelector("h9").textContent = item.height;
+        newSeccion.querySelector("h10").textContent = item.weight;
         fragment.appendChild(newSeccion);  
           
     });
-    document.getElementById("athletes").style.display="none"; //para ocultar el primer elmento
+    document.getElementById("athlete").style.display="none"; //para ocultar el primer elmento
     parentMain.appendChild(fragment); //devuelves el fragment al final del documento   
