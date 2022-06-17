a55397da7163f1eec791617d71f7d9967e442f1d
Vanessa <vanessa@MacBook-Air-de-Vanessa.local>
2022-06-17 12:12:14 -0500

    creando funciones en data.js

--- src/data.js
+++ src/data.js
@@ -1,12 +1,63 @@
-// estas funciones son de ejemplo
+
+export function newDataNoRepeat(data){
+  data.map(function(objeto) {
+         delete objeto.medal;
+         delete objeto.event;
+       }) 
+ const DataObjectText = new Set( data.map( JSON.stringify ) );
+ const DataUniqObject = Array.from(DataObjectText).map( JSON.parse );
+ return DataUniqObject;
+} 
+
+export function filterData(data,condition) {
+  return data.filter ((item)=>(item.gender===condition))
+}
 
 
 
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+
+// export function noRepeat(datos1){
+//     let i=0;
+//     while(datos1[i].name==datos1[i+1].name){    
+//       let obj2={medal: `${datos1[i].medal}, ${datos1[i+1].medal}`}; 
+//       let obj3={event: `${datos1[i].event}, ${datos1[i+1].event}`};
+//       let obj1=datos1[i];
+//       let obj4=Object.assign(obj1, obj2, obj3);
+//       datos1.splice(i+1,1);
+//       if (datos1[i].name==datos1[i+1].name) {i=0} else {i++}
+//     } 
+//      return datos1
+//     }
+
+
+
+
+
+// export function noRepeat2 (datos1){
+//         let i =0;
+//         if (datos1[i].name==datos1[i+1].name) {i=0} else  {i++};
+//           while(datos1[i].name==datos1[i+1].name){  
+//           let obj2={medal: `${datos1[i].medal}, ${datos1[i+1].medal}`}; 
+//           let obj3={event: `${datos1[i].event}, ${datos1[i+1].event}`};
+//           let obj1=datos1[i];
+//           Object.assign(obj1, obj2, obj3);
+//           datos1.splice(i+1,1);
+//           i++
+//         } 
+//          return datos1
+//         }  
+
+//         function newData (datos1){
+        
+//           datos1.map(function(objeto){
+//             delete objeto.medal;
+//             delete objeto.event;
+           
+//           })
+//           //datos1.map(objeto => delete objeto.event); 
+//           return datos1;
+//           }  
+//    console.log(newData (datos3))        
--- src/main.js
+++ src/main.js
@@ -1,46 +1,18 @@
 import data from './data/athletes/athletes.js';
-console.log(data);
+import {filterData} from './data.js';
+import {newDataNoRepeat} from './data.js';
+//import {noRepeat2} from './data.js';
+// import { getItemDescriptor } from '@babel/core/lib/config/item';
+//  console.log(data);
 
-//menu
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
+const datos =data.athletes;
+//console.log(datos);
 
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("nav-menu_visible");
-  if (navMenu.classList.contains("nav-menu_visible")) {
-    navToggle.setAttribute("aria-label", "Cerrar menú");
-    console.log(navToggle);
-  } else {
-    navToggle.setAttribute("aria-label", "Abrir menú");
-  }
-});
+const dataNoRepeat=newDataNoRepeat(datos);
+//console.log(dataNoRepeat)
 
-    const fragment=document.createDocumentFragment();
-    //Crear nuevo div y añadirlo al elemento main
-    const newDiv = document.createElement("div"); // crea un nuevo div
-    const newContent = document.createTextNode("El número total de deportistas es:");  // y añade contenido
-    newDiv.appendChild(newContent); //añade texto al div creado.
-    // añade el elemento creado y su contenido al DOM
-    const elementMain = document.getElementById("athlete"); // con el metodo getElementById devuelve una referencia del elemento seccion con id "athletes" (es traido del DOM)
-    const parentMain = elementMain.parentNode; // Obtener una referencia al nodo 
-    parentMain.insertBefore(newDiv, elementMain); // 
-    
-    data.athletes.forEach((item) => {
-        //Clonar una seccion del DOM y añadirla
-        //añade secciones clonadas del elemento seccion athletes
-        const newSeccion = elementMain.cloneNode(true); //clonamos un nodo del DOM (el nodo es de la seccion existente) -TRUE es para clonar los hijos de ese nodo
-        //parentMain.appendChild(newSeccion); // clonas un documeto y lo devuelves al final 
-        //console.log(item)
-        newSeccion.querySelector("h2").textContent = item.team;
-        newSeccion.querySelector("h3").textContent = item.name;
-        newSeccion.querySelector("h4").textContent = item.medal;
-        newSeccion.querySelector("h5").textContent = item.sport;
-        newSeccion.querySelector("h6").textContent = item.gender;
-        newSeccion.querySelector("p").textContent = `
-        ${item.name} (${item.team})
-        Peso: ${item.height} y Altura: ${item.weight}`; 
-        fragment.appendChild(newSeccion);  
-          
-    });
-    document.getElementById("athlete").style.display="none"; //para ocultar el primer elmento
-    parentMain.appendChild(fragment); //devuelves el fragment al final del documento   
+const dataFemale=filterData(dataNoRepeat, "F");
+//console.log(dataFemale)
+
+const dataMale=filterData(dataNoRepeat, "M");
+console.log(dataMale)
