84af4dd5bac63e8e23a19620532235fd1182fe39
Olga Acosta <olgaacostab@gmail.com>
2022-06-17 17:25:12 -0500

    Ya creado el botón para ver los atletas sin datos repetidos

--- src/data.js
+++ src/data.js
@@ -1,10 +1,17 @@
+// export const example = () => {
+//   return 'example';
+// };
 
-// estas funciones son de ejemplo
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+export function noRepeatedData(datos){
+  datos.map(function(object) {
+    delete object.medal;
+    delete object.event;
+  })
+  const dataObjectText = new Set(datos.map(JSON.stringify));
+  const dataUniqueObject = Array.from(dataObjectText).map(JSON.parse);
+  return dataUniqueObject;
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import data from './data/athletes/athletes.js';
 //console.log(data.athletes);
 
+import {noRepeatedData} from "./data.js";
+const dataRio = data.athletes;
+const datos1 = noRepeatedData(dataRio);
+//console.log(datos1)
+
 //menu
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
@@ -20,7 +25,7 @@ navToggle.addEventListener("click", () => {
 const femenino =() =>{
 return data1.filter ((fem)=>(fem.gender==="F"))
 }
-  console.log(femenino()); */
+console.log(femenino()); */
 
 //const principalSection = document.createElement("section"); //esta sección es para incluir todos los datos
 //const fragment = document.createDocumentFragment();
@@ -32,24 +37,30 @@ newDiv.appendChild(newContent); //añade texto al div creado.
 const elementMain = document.getElementById("athlete"); // con el metodo getElementById devuelve una referencia del elemento seccion con id "athletes" (es traido del DOM)
 const parentMain = elementMain.parentNode; // Obtener una referencia al nodo 
 parentMain.insertBefore(newDiv, elementMain); //
-    
 // const newSeccion = elementMain.cloneNode(true);
 
-data.athletes.forEach((item) => {
-  //Clonar una seccion del DOM y añadirla
-  //añade secciones clonadas del elemento seccion athletes
-  const newSeccion = elementMain.cloneNode(true); //clonamos un nodo del DOM (el nodo es de la seccion existente) -TRUE es para clonar los hijos de ese nodo
-  //parentMain.appendChild(newSeccion); // clonas un documeto y lo devuelves al final 
-  //console.log(item)
-  newSeccion.querySelector("h2").textContent = item.team;
-  newSeccion.querySelector("h3").textContent = item.name;
-  newSeccion.querySelector("h4").textContent = item.medal;
-  newSeccion.querySelector("h5").textContent = item.sport;
-  newSeccion.querySelector("h6").textContent = item.gender;
-  newSeccion.querySelector("p").textContent = `${item.name} (${item.team}) Peso: ${item.height} y Altura: ${item.weight}`; 
-  // parentMain.appendChild(principalSection);
+const athletesButton = document.getElementById("allAthletes");
 
-parentMain.appendChild(newSeccion);
-});
-// document.getElementById("athlete").style.display="none"; //para ocultar el primer elmento
+athletesButton.addEventListener('click', showAthletes);
+function showAthletes(){
+  datos1.forEach((item) => {
+    //Clonar una seccion del DOM y añadirla
+    //añade secciones clonadas del elemento seccion athletes
+    const newSeccion = elementMain.cloneNode(true); //clonamos un nodo del DOM (el nodo es de la seccion existente) -TRUE es para clonar los hijos de ese nodo
+    //parentMain.appendChild(newSeccion); // clonas un documeto y lo devuelves al final 
+    //console.log(item)
+    const prueba = document.getElementById("pais").textContent;
+    newSeccion.prueba = item.team;
+    newSeccion.querySelector("h3").textContent = item.name;
+    newSeccion.querySelector("h4").textContent = item.sport;
+    newSeccion.querySelector("h5").textContent = item.gender;
+    newSeccion.querySelector("h6").textContent = `${item.name} (${item.team}) Peso: ${item.height} y Altura: ${item.weight}`;
+    //newSeccion.querySelector("p").textContent = `${item.name} (${item.team}) Peso: ${item.height} y Altura: ${item.weight}`; 
+    // parentMain.appendChild(principalSection);
+  
+  parentMain.appendChild(newSeccion);
+  });
+  document.getElementById("athlete").style.display="none"; //para ocultar el primer elmento
+}
+
 //parentMain.appendChild(newSeccion); //devuelves el fragment al final del documento
