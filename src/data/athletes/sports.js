a2c331271313a3a25dd8f188b431f8b64b9f9c34
Vanessa <vanessa@MacBook-Air-de-Vanessa.local>
2022-07-09 20:52:08 -0500

    lista de destacados

--- src/data/athletes/sports.js
+++ src/data/athletes/sports.js
@@ -70,3 +70,39 @@ export default{
       "sport": "Golf"},
   ]
 }
+// 0 {athlete: "Michael Fred Phelps, II", total: 6, gold: 5, silver: 1, bronze: 0}
+// 1 {athlete: "Kathleen Genevieve \"Katie\" Ledecky", total: 5, gold: 4, silver: 1, bronze: 0}
+// 2 {athlete: "Simone Arianne Biles", total: 5, gold: 4, silver: 0, bronze: 1}
+// 3 {athlete: "Danuta Kozk", total: 3, gold: 3, silver: 0, bronze: 0}
+// 4 {athlete: "Jason Francis Kenny", total: 3, gold: 3, silver: 0, bronze: 0}
+// 5 {athlete: "Katinka Hossz", total: 4, gold: 3, silver: 1, bronze: 0}
+// 6 {athlete: "Ryan Murphy", total: 3, gold: 3, silver: 0, bronze: 0}
+// 7 {athlete: "Usain St. Leo Bolt", total: 3, gold: 3, silver: 0, bronze: 0}
+// 8 {athlete: "Allyson Michelle Felix", total: 3, gold: 2, silver: 1, bronze: 0}
+// 9 {athlete: "Anthony Lee Ervin", total: 2, gold: 2, silver: 0, bronze: 0}
+// 10 {athlete: "Caeleb Remel Dressel", total: 2, gold: 2, silver: 0, bronze: 0}
+// 11 {athlete: "Chang Hye-Jin", total: 2, gold: 2, silver: 0, bronze: 0}
+// 12 {athlete: "Chen Aisen", total: 2, gold: 2, silver: 0, bronze: 0}
+// 13 {athlete: "Ding Ning", total: 2, gold: 2, silver: 0, bronze: 0}
+// 14 {athlete: "Elaine Thompson", total: 3, gold: 2, silver: 1, bronze: 0}
+// 15 {athlete: "Gabriella Timea Szab", total: 2, gold: 2, silver: 0, bronze: 0}
+// 16 {athlete: "Kohei Uchimura", total: 2, gold: 2, silver: 0, bronze: 0}
+// 17 {athlete: "Ku Bon-Chan", total: 2, gold: 2, silver: 0, bronze: 0}
+// 18 {athlete: "Laura Rebecca Trott (-Kenny)", total: 2, gold: 2, silver: 0, bronze: 0}
+// 19 {athlete: "Lilly King", total: 2, gold: 2, silver: 0, bronze: 0}
+// 20 {athlete: "Ma Long", total: 2, gold: 2, silver: 0, bronze: 0}
+// 21 {athlete: "Madeline Jane \"Maya\" DiRado", total: 4, gold: 2, silver: 1, bronze: 1}
+// 22 {athlete: "Marcus Gro", total: 2, gold: 2, silver: 0, bronze: 0}
+// 23 {athlete: "Max Antony Whitlock", total: 3, gold: 2, silver: 0, bronze: 1}
+// 24 {athlete: "Max Rendschmidt", total: 2, gold: 2, silver: 0, bronze: 0}
+// 25 {athlete: "Mohamed Muktar Jama \"Mo\" Farah", total: 2, gold: 2, silver: 0, bronze: 0}
+// 26 {athlete: "Nataliya Sergeyevna Ishchenko", total: 2, gold: 2, silver: 0, bronze: 0}
+// 27 {athlete: "Nathan Ghar-Jun Adrian", total: 4, gold: 2, silver: 0, bronze: 2}
+// 28 {athlete: "Niccol Campriani", total: 2, gold: 2, silver: 0, bronze: 0}
+// 29 {athlete: "Sebastian Brendel", total: 2, gold: 2, silver: 0, bronze: 0}
+// 30 {athlete: "Shi Tingmao", total: 2, gold: 2, silver: 0, bronze: 0}
+// 31 {athlete: "Simone Ashley Manuel", total: 4, gold: 2, silver: 2, bronze: 0}
+// 32 {athlete: "Svetlana Alekseyevna Romashina", total: 2, gold: 2, silver: 0, bronze: 0}
+// 33 {athlete: "Tianna Madison-Bartoletta", total: 2, gold: 2, silver: 0, bronze: 0}
+// 34 {athlete: "Yana Karapetovna Yegoryan", total: 2, gold: 2, silver: 0, bronze: 0}
+// 35 {athlete: "Abbey Weitzeil", total: 2, gold: 1, silver: 1, bronze: 0}
